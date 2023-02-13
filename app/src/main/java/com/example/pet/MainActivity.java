3b50c0d77c790fc411ac46a47d44114ff5b908be
ivanovv <aa2652839@gmail.com>
2023-02-13 13:59:36 +0900

    123

--- app/src/main/java/com/example/pet/MainActivity.java
+++ app/src/main/java/com/example/pet/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.pet;
 
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.view.View;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.List;
 import android.widget.EditText;
 import android.widget.ProgressBar;
@@ -29,7 +33,6 @@ public class MainActivity extends AppCompatActivity {
     EditText TagsId;
     EditText TagsName;
     EditText Status;
-    ProgressBar loadingPB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,66 +57,35 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(MainActivity.this, "Пожалуйста заполните все поля!", Toast.LENGTH_SHORT).show();
             return;
         }
-        // calling a method to post the data and passing our name and job.
         postData(Id.getText().toString(), Integer.parseInt(CategoryId.getText().toString()), CategoryName.getText().toString(), Name.getText().toString(),
                 PhotoUrl.getText().toString(), Integer.parseInt(TagsId.getText().toString()), TagsName.getText().toString(), Status.getText().toString());
     }
 
     private void postData(String id, Integer categoryId, String categoryName, String name, String photoUrl, Integer tagsId, String tagsName, String status) {
-
-        // below line is for displaying our progress bar.
-        loadingPB.setVisibility(View.VISIBLE);
-
-        // on below line we are creating a retrofit
-        // builder and passing our base url
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://petstore.swagger.io/v2/")
-                // as we are sending data in json format so
-                // we have to add Gson converter factory
                 .addConverterFactory(GsonConverterFactory.create())
-                // at last we are building our retrofit builder.
                 .build();
-        // below line is to create an instance for our retrofit api class.
         PetAPI petAPI = retrofit.create(PetAPI.class);
+        ArrayList<Tags> tags = new ArrayList<>();
+        Tags tag = new Tags(tagsId, tagsName);
+        tags.add(tag);
 
-        // passing data from our text fields to our modal class.
-        Pet modal = new Pet(id, new Category(categoryId, categoryName), name, photoUrl, List<new Tags(tagsId, tagsName)>, status);
-
-        // calling a method to create a post and passing our modal class.
+        ArrayList<String> urls = new ArrayList<>();
+        urls.add(photoUrl);
+        Pet modal = new Pet(Integer.parseInt(id), new Category(categoryId, categoryName), name, urls, tags, status);
         Call<Pet> call = petAPI.createPost(modal);
-
-        // on below line we are executing our method.
         call.enqueue(new Callback<Pet>() {
             @Override
             public void onResponse(Call<Pet> call, Response<Pet> response) {
-                // this method is called when we get response from our api.
                 Toast.makeText(MainActivity.this, "Data added to API", Toast.LENGTH_SHORT).show();
-
-                // below line is for hiding our progress bar.
-                loadingPB.setVisibility(View.GONE);
-
-                // on below line we are setting empty text
-                // to our both edit text.
-                jobEdt.setText("");
-                nameEdt.setText("");
-
-                // we are getting response from our body
-                // and passing it to our modal class.
-                Pet responseFromAPI = response.body();
-
-                // on below line we are getting our data from modal class and adding it to our string.
-                String responseString = "Response Code : " + response.code() + "\nName : " + responseFromAPI.getName() + "\n" + "Job : " + responseFromAPI.getJob();
-
-                // below line we are setting our
-                // string to our text view.
-                responseTV.setText(responseString);
+                String responseString = "Response Code : " + response.code();
+                Answer.setText(responseString);
             }
 
             @Override
             public void onFailure(Call<Pet> call, Throwable t) {
-                // setting text to our text view when
-                // we get error response from API.
-                responseTV.setText("Error found is : " + t.getMessage());
+                Answer.setText("Error found is : " + t.getMessage());
             }
         });
     }
